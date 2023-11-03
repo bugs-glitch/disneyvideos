d638002ad90e8dff863c7bf0dc416e153a0763a4
bugs/looner <132413115+bugs-glitch@users.noreply.github.com>
2023-11-03 16:37:34 -0400

    Add files via upload

---
+++ animatedfilms/wall-e/games/sayit/js/popupAndTracking.js
@@ -0,0 +1,10 @@
+function simpleTrack (sID) {
+	/* this is removed b/c omniture & hbx tracking is run from launching website rather than this pop-up
+	_hbflash(sID,'n','n','n');
+	*/
+}
+function jSimpleTrack (sID) {
+	/* ditto
+	_hbflash(sID,'n','n','n');
+	*/
+}
